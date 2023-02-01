57e964e36c6717b33a490fe7a76b9662fe443e01
dronezzzko <andrey.gadyukov@gmail.com>
2023-02-02 00:04:25 +0200

    feat(main|api): readable tests

--- internal/api/handlers_test.go
+++ internal/api/handlers_test.go
@@ -29,22 +29,12 @@ This song is gonna be sung first time!
 		{
 			name:       "second API call for en-US",
 			langHeader: "en-US",
-			response: `10 green bottles standing on a wall,
-And if 1 green bottle should accidentally fall,
-There’ll be 9 green bottles standing on a wall.
-
-This song has already been sung 1 time!
-`,
+			response:   `This song has already been sung 1 time!`,
 		},
 		{
 			name:       "third API call for en-US",
 			langHeader: "en-US",
-			response: `10 green bottles standing on a wall,
-And if 1 green bottle should accidentally fall,
-There’ll be 9 green bottles standing on a wall.
-
-This song has already been sung 2 times!
-`,
+			response:   `This song has already been sung 2 times!`,
 		},
 		{
 			name:       "API call for fr-FR",
@@ -59,22 +49,13 @@ Cette chanson a déjà été chantée 3 fois !
 		{
 			name:       "unsupported language",
 			langHeader: "de-DE",
-			response: `10 green bottles standing on a wall,
-And if 1 green bottle should accidentally fall,
-There’ll be 9 green bottles standing on a wall.
-
-This song has already been sung 4 times!
+			response: `This song has already been sung 4 times!
 `,
 		},
 		{
 			name:       "invalid header",
 			langHeader: "243242!",
-			response: `10 green bottles standing on a wall,
-And if 1 green bottle should accidentally fall,
-There’ll be 9 green bottles standing on a wall.
-
-This song has already been sung 5 times!
-`,
+			response:   `This song has already been sung 5 times!`,
 		},
 	}
 
@@ -84,9 +65,8 @@ This song has already been sung 5 times!
 			req := httptest.NewRequest(http.MethodGet, "/", nil)
 			req.Header.Add("Accept-Language", tc.langHeader)
 			rec := httptest.NewRecorder()
-
 			assert.NoError(t, Song(&LanguageContext{e.NewContext(req, rec)}))
-			assert.Equal(t, tc.response, rec.Body.String())
+			assert.Contains(t, rec.Body.String(), tc.response)
 		})
 	}
 }
