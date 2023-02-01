032d4401b951b94a18b0ae0f17554e047c432a9d
dronezzzko <andrey.gadyukov@gmail.com>
2023-02-01 02:27:37 +0200

    fix(main): comments

--- internal/localizer/printer/printer.go
+++ internal/localizer/printer/printer.go
@@ -12,7 +12,7 @@ import (
 const defaultLocaleID = "en-US"
 
 type (
-	// internalLocaleID is service internal language ID.
+	// internalLocaleID is service's internal language ID.
 	internalLocaleID string
 	// localeID is BCP 47 language ID.
 	localeID string
@@ -23,8 +23,8 @@ var locales = map[internalLocaleID]localeID{
 	"fr-fr": "fr-FR",
 }
 
-// Make instantiates a new Localizer for provided locale ID.
-// If ID is not supported a localizer for default locale (defaultLocaleID) will be returned.
+// Make instantiates a new message.Printer for provided locale ID.
+// If ID is not supported a message.Printer for default locale (defaultLocaleID) will be returned.
 func Make(ID string) (*message.Printer, bool) {
 	normalizedID := internalLocaleID(strings.ToLower(ID))
 	if langID, ok := locales[normalizedID]; ok {
